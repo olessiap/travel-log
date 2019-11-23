dfd8963e5793cc864fbe62ba411fc1c08f516956
olessiap <olessia.potapova@gmail.com>
2019-11-22 21:37:01 -0300

    added home country page placeholder

---
+++ src/CountryPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class CountryPage extends Component {
+  render() {
+    return (
+      <div className="main">
+        <h1 className="country-title">MEXICO</h1>
+        <div className="footer">
+          <div className="add-button">+</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CountryPage
